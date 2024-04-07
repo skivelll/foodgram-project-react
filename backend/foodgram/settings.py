12f07cad6b9779ea2c3ab6f61e6ba2d9f0953ae1
Arseniy <skivelll@yandex.ru>
2024-04-07 23:24:00 +0300

    a

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -23,8 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-
 SECRET_KEY = os.getenv('SECRET_KEY', 'default-empty-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
